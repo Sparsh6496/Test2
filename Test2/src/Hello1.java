d4a9e7d4032a0574612510157bbcf4f7e50e8498
Sparsh Bansal <sparsh6496@gmail.com>
2017-02-08 15:02:19 +0530

    Added sum function

--- Test2/src/Hello1.java
+++ Test2/src/Hello1.java
@@ -6,7 +6,11 @@ public class Hello1 {
 System.out.println("this is a hello world program");
 System.out.println("A new change has been made");
 System.out.println("A new change has been made again");
-
+int a ,b,c;
+		a=5;
+		b=6;
+		c=a+b;
+		System.out.println("The sum of two numbers is :" +c);
 
 
 }
